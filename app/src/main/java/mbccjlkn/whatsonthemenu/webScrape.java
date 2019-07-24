66f1978f33b44fbf2b1f21fe98fd238c573ef8e6
Zhengyang Zhang <50434360+wrAres@users.noreply.github.com>
2019-07-23 21:36:12 -0700

    it reads local file

--- app/src/main/java/mbccjlkn/whatsonthemenu/webScrape.java
+++ app/src/main/java/mbccjlkn/whatsonthemenu/webScrape.java
@@ -1,40 +1,50 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import java.io.FileWriter;
+import java.io.FileWriter; 
+import java.util.Scanner;
+import java.io.*; 
+
 
 public class webScrape {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		String DH[] = new String[5];
 		String Name[] = new String[5];
-		DH[0] =
-				"https://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=20&locationName=Crown+Merrill+Dining+Hall&sName=&naFlag=";
-		DH[1] =
-				"https://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=25&locationName=Porter+Kresge+Dining+Hall&sName=&naFlag=";
-		DH[2] =
-				"https://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=30&locationName=Rachel+Carson+Oakes+Dining+Hall&sName=&naFlag=";
-		DH[3] =
-				"https://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=40&locationName=Colleges+Nine+%26+Ten+Dining+Hall&sName=&naFlag=";
-		DH[4] =
-				"https://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=05&locationName=Cowell+Stevenson+Dining+Hall&sName=&naFlag=";
+		File files[] = new File[5];
+		//Scanner sc = new Scanner(System.in);
+		
+		
+        DH[0] = 
+				"crown.html";                
+		DH[1] = 
+                "porter.html";
+		DH[2] = 
+                "oakes.html";
+		DH[3] = 
+                "c9c10.html";
+		DH[4] = 
+                "cowell.html";
 		Name[0] = "Crown&Merrill";
 		Name[1] = "Porter&Kresge";
 		Name[2] = "RachelCarson&Oakes";
 		Name[3] = "Nine&Ten";
 		Name[4] = "Cowell&Stevenson";
-
-		try {
+		for(int i = 0;i<5;i++){
+			files[i] = new File(DH[i]);
+		}
+        try {
 			FileWriter fw = new FileWriter("data.txt");
 			for(int i = 0;i<5;i++){
-
-				final Document document = Jsoup.connect(DH[i]).get();
-
-
+				
+			//	final Document document = Jsoup.connect(DH[i]).timeout(12000).get();
+				Document doc = Jsoup.parse(files[i],null);
+				System.out.println(doc);
+				
 				fw.write("==========================");
 				fw.write(Name[i]);
 				fw.write("==========================\n");
-
-				for (Element food : document.select("div.menusamprecipes,div.menusampmeals,img[src$=.gif]")) {
+				
+				for (Element food : doc.select("div.menusamprecipes,div.menusampmeals,div.shortmenurecipes,div.shortmenumeals,img[src$=.gif]")) {
 					if(food.text().equals("Breakfast")||food.text().equals("Lunch")||food.text().equals("Dinner")){
 						fw.write("\n");
 					}
@@ -42,22 +52,22 @@ public class webScrape {
 					if (food.text().equals("")){  //finds the ingredients of the food
 						String tag = food.attr("src");
 						fw.write("------");
-						fw.write( tag.substring(tag.indexOf('/')+1,tag.indexOf('.')));
+						fw.write( tag.substring(tag.indexOf('_')+7,tag.length()-4));
 					}
 					fw.write("\n");
 					if(food.text().equals("Breakfast")||food.text().equals("Lunch")||food.text().equals("Dinner")){
 						fw.write("\n");
-					}
+					}				
 				}
-
+				
 			}
 			fw.close();
-
-		}
-		catch (Exception ex) {
-			ex.printStackTrace();
-		}
-
-	}
-
+			
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+    
 }
